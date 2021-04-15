f00205b7eba502f4ffb35540d21a3e4d6aef7813
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2021-04-15 08:46:02 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,12 +1,24 @@
-
-class test
-{
-	public static void main(String[] args)
-	{
-		int a, b, c;
-		a = 50;
-		b = 50;
-		c = a+b;
-		System.out.println(c);
-	}
+package Practice;
+import java.io.*;
+import java.util.*;
+class test {
+    public static void main(String[] args) {
+        if(args.length==1){
+            String fileName = args[0];
+            TreeSet<String> set = new TreeSet<>();
+            File file = new File(fileName);
+            try {
+                Scanner s = new Scanner(file);
+                while (s.hasNext()){
+                    set.add(s.next());
+                }
+                System.out.println(set);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            System.out.println("Please, Pass the File Name as Command Line Argument");
+        }
+    }
 }
